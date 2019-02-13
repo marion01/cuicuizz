8138a3d5b2a3f65fb57d9e4c0fd176265b612fcf
fawalczysz <fabien.walczyszyn@etu.uca.fr>
2019-02-13 15:04:45 +0100

    initial project

---
+++ src/main/java/fr/isima/cuicuizz/CuicuizzApplication.java
@@ -0,0 +1,14 @@
+package fr.isima.cuicuizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuicuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuicuizzApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/fr/isima/cuicuizz/CuicuizzApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.isima.cuicuizz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CuicuizzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
